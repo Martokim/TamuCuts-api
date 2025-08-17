3b3220280fc61b5a2c02ac99cf97b5bf6cf457b4
Marto <martinkimani42@gmail.com>
2025-08-17 18:57:42 +0300

    feat(models): add Order and OrderItem models

--- butchery/models.py
+++ butchery/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings 
 
 class User(AbstractUser):
     # Extra fields for TamuCuts users
@@ -25,9 +26,41 @@ class Product(models.Model):
     category = models.CharField(max_length=200, choices=CATEGORY_CHOICES)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock_quantity = models.PositiveIntegerField(default=0)
-    descritpion = models.TextField(blank=True, null=True)
+    description = models.TextField(blank= True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} ({self.category}) - ${self.price}"
+    
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELLED', 'Cancelled'), 
+    ]
+    
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES, default='PENDING'  )
+     
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+    def get_total_price(self):
+        return self.quantity * self.product.price

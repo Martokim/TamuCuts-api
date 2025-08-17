6f992f5bad5becc7e58f7c4c2acac2db7b428bdf
Marto <martinkimani42@gmail.com>
2025-08-17 18:58:14 +0300

    chore(admin): register Order and OrderItem with inline support

--- butchery/admin.py
+++ butchery/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Product
+from .models import User,Product,Order,OrderItem
 
 
 @admin.register(User)
@@ -17,3 +17,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ("category","created_at")
     search_fields = ("name", "category")
     ordering = ("-created_at",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "status", "created_at", "updated_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("customer__username",)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity")
+    search_fields = ("product__name",)
+    

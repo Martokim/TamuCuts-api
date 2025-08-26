6ad39c58d9a56ef6a64631575f66c57d565e1e77
Marto <martinkimani42@gmail.com>
2025-08-26 12:56:53 +0300

    feat(api):add crud functionality for my models

--- butchery/views.py
+++ butchery/views.py
@@ -1,7 +1,23 @@
-from .models import User
-from .serializers import UserSerializer
 from rest_framework import viewsets
+from .models import User, Product, Order, OrderItem
+from .serializers import UserSerializer, ProductSerializer, OrderSerializer, OrderItemSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class =UserSerializer
+    serializer_class = UserSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
